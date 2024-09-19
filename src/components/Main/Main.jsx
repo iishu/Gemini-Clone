aa8099deec1178ecd7ecd87e7a143ed0c22e5471
iishu <ishikasrivastava27594@gmail.com>
2024-09-19 20:12:03 +0530

    added Gemini.js

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
         </div>
         <div className="main-container">
           <div className="greet">
-            <p><sapn>Hello, Dev.</sapn></p>
+            <p><span>Hello, Dev.</span></p>
             <p>How can I help you today?</p>
           </div>
           <div className="cards">
@@ -31,6 +31,19 @@ const Main = () => {
               <img src={assets.code_icon} alt="" />
             </div>
             
+          </div>
+          <div className="main-bottom">
+            <div className="search-box">
+              <input type="text" placeholder="Enter a prompt here"/>
+              <div>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                <img src={assets.send_icon} alt="" />
+              </div>
+            </div>
+            <p className="bottom-info">
+              Gemini may display inaccurate info, including about people, so double-check its response. Your privacy and Gemini Apps.
+            </p>
           </div>
         </div>
     </div>
---
+++ src/config/Gemini.js
@@ -0,0 +1,42 @@
+ 
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+// Create a single instance of the API key
+const API_KEY = "AIzaSyBqsGEzwAMzlPB1vSfuRanVFS-ZdaRID7Q";
+
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function runChat(prompt) {
+  const chatSession = model.startChat({
+    generationConfig, // safetySettings: Adjust safety settings // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+}
+
+export default runChat;
