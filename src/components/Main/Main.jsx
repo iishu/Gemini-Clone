2a327bac887148e2105af0ae9659afd7d73a59ad
iishu <ishikasrivastava27594@gmail.com>
2024-09-21 23:15:45 +0530

    added loader property

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
     onSent,
     recentPrompt,
     showResult,
-    loadings,
+    loading,
     resultData,
     setInput,
     input,
@@ -57,9 +57,16 @@ const Main = () => {
             </div>
             <div className="result-data">
               <img src={assets.gemini_icon} alt="" />
-              <p dangerouslySetInnerHTML={{ __html: resultData }}>
+              {loading ?<div className="loader">
+                 <hr />
+                 <hr />
+                 <hr />
+              </div>
+              : <p dangerouslySetInnerHTML={{ __html: resultData }}>
                  
               </p>
+              }
+             
             </div>
           </div>
         )}
