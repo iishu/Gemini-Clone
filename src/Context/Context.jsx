95ac9cc2f7bed0ce933ca6bab9574600a2940203
iishu <ishikasrivastava27594@gmail.com>
2024-09-22 23:00:34 +0530

    added response to the context

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -19,7 +19,19 @@ const ContextProvider=(props)=>{
         setShowResult(true)
         setRecentPrompt(input)
         const response=await runChat(input)
-        setResultData(response)
+        let responseArray=response.split("**")
+        let newResponse;
+        for(let i=0;i<responseArray.length;i++)
+        {
+            if(i==0||i%2!==1){
+               newResponse+=responseArray[i] ;
+
+            }
+            else{
+                newResponse+="<b>"+responseArray[i]+"</b>"
+            }
+        }
+        setResultData(newResponse)
         setLoadings(false)
         setInput("")
 
